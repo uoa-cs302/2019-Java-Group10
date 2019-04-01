92712c9c13d8f796dfdb25d9eb1513ca4ce0a0d0
lakshyaoberoi <lobe655@aucklanduni.ac.nz>
2019-04-01 16:40:00 +1300

    new test files

---
+++ test.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class test extends JFrame{
+	public test(){
+
+		super("TestLabel");
+		setSize (300, 400);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		JLabel pageLabel = new JLabel("This is my label. Do not touch");
+		FlowLayout flo = new FlowLayout(-1, 50, 50);
+		setLayout(flo);
+		add(pageLabel);
+		setVisible(true);
+	}
+	public static void main(String[] arguments){
+		test testLabel = new test();
+	}
+}
