6c13c900364bf368d038d715dd9fce70ba68f066
DevashishDhyani <devashishdhyani@gmail.com>
2019-04-01 16:45:41 +1300

    New updates

--- test.java
+++ test.java
@@ -7,11 +7,12 @@ public class test extends JFrame{
 		super("TestLabel");
 		setSize (300, 400);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		JLabel pageLabel = new JLabel("This is my label. Do not touch");
+		JLabel pageLabel = new JLabel("This is my label. Do not touch. OK");
 		FlowLayout flo = new FlowLayout(-1, 50, 50);
 		setLayout(flo);
 		add(pageLabel);
 		setVisible(true);
+		
 	}
 	public static void main(String[] arguments){
 		test testLabel = new test();
