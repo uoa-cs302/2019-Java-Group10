ce36ea45c482a855a2f03d71da5b2eb1f1b7451d
lakshyaoberoi <lobe655@aucklanduni.ac.nz>
2019-04-02 00:33:46 +1300

    second upload

--- Home.java
+++ Home.java
@@ -1,19 +1,45 @@
 import javax.swing.*;
 import java.awt.*;
 
-public class test extends JFrame{
-	public test(){
+public class Home extends JFrame{
+	public Home(){
+		super("Zelda Reborn");
+		setSize (1024, 768);
 
-		super("TestLabel");
-		setSize (300, 400);
+		JFrame frame = new JFrame();
+
+
+		ImageIcon backgroundImage = new ImageIcon(getClass().getResource("ZeldaBackground.jpg"));
+		Image background1 = backgroundImage.getImage();
+		//rescale image to fit dimensions of the frame
+		Image background2 = background1.getScaledInstance(1024, 768, Image.SCALE_DEFAULT);
+		ImageIcon background3 = new ImageIcon(background2);
+		JLabel backgroundPicture = new JLabel(background3);
+
+
+		backgroundPicture.setLayout(new BorderLayout());
+ 		
+	
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		JLabel pageLabel = new JLabel("This is my label. Do not touch");
-		FlowLayout flo = new FlowLayout(-1, 50, 50);
-		setLayout(flo);
-		add(pageLabel);
+		JLabel pageLabel = new JLabel("ZELDA Reborn");
+
+			
+		pageLabel.setFont(new Font("Arial", Font.PLAIN, 75));
+		pageLabel.setForeground(Color.WHITE);
+		//backgroundPicture.add(pageLabel);
+		
+		//FlowLayout flo = new FlowLayout(1, 50, 50);
+		//pageLabel.setLayout(flo);
+
+		backgroundPicture.add(pageLabel, BorderLayout.NORTH);
+		
+		add(backgroundPicture);		
+
+		setResizable(false);
 		setVisible(true);
+		
 	}
 	public static void main(String[] arguments){
-		test testLabel = new test();
+		Home background = new Home();
 	}
 }
