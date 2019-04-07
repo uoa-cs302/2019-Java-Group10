f63a4cbaf28c1d9d4e8cf0db473260b21775ec4b
DevashishDhyani <devashishdhyani@gmail.com>
2019-04-07 23:04:13 +1200

    movement step changed

--- ZeldaCharacter.java
+++ ZeldaCharacter.java
@@ -66,19 +66,19 @@ public class ZeldaCharacter {
         int key = e.getKeyCode();
 
         if (key == KeyEvent.VK_LEFT) {
-            dx = -2;
+            dx = -1;
         }
 
         if (key == KeyEvent.VK_RIGHT) {
-            dx = 2;
+            dx = 1;
         }
 
         if (key == KeyEvent.VK_UP) {
-            dy = -2;
+            dy = -1;
         }
 
         if (key == KeyEvent.VK_DOWN) {
-            dy = 2;
+            dy = 1;
         }
     }
 
