138d59421587ea01fcf28fed66c4fe81bf17fd14
Lakshya Oberoi <lobe655@aucklanduni.ac.nz>
2019-04-11 11:11:14 +1200

    new changes

--- test.java
+++ test.java
@@ -1,20 +1,73 @@
 import javax.swing.*;
 import java.awt.*;
+import java.io.*;
 
-public class test extends JFrame{
-	public test(){
 
-		super("TestLabel");
-		setSize (300, 400);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		JLabel pageLabel = new JLabel("This is my label. Do not touch. OK");
-		FlowLayout flo = new FlowLayout(-1, 50, 50);
-		setLayout(flo);
-		add(pageLabel);
-		setVisible(true);
+public class test extends JFrame {
+
+	//this function creates JPanels that create the boundary of the level in the form of trees 
+	private void levelBorder(JPanel border, String direction){
+		
+		border.setBackground(new Color(56,187,77));
+		
+		//based on the direction, we align the trees to the respective side of the JPanel
+		if (direction == "EAST") {
+			add(border, BorderLayout.EAST);	
+		}
+		else if (direction == "WEST") {
+			add(border, BorderLayout.WEST);
+		}
+		else if (direction == "NORTH") {
+			add(border, BorderLayout.NORTH);
+		}
+		else if (direction == "SOUTH") {
+			add(border, BorderLayout.SOUTH);
+		}
+		
+		//this creates an x amount of trees defined by its direction
+		if ((direction == "EAST") || (direction == "WEST")) {
+			border.setLayout(new GridLayout(15, 10, 0, 0));
+			for(int i = 0; i< 15; i++){
+				JLabel tree = new JLabel(new ImageIcon("tree.png"));
+				border.add(tree);
+			}
+		}
+		if ((direction == "NORTH") || (direction == "SOUTH")) {
+			border.setLayout(new GridLayout(1, 25, 0, 0));
+			for(int i = 0; i< 25; i++){
+				JLabel tree = new JLabel(new ImageIcon("tree.png"));
+				border.add(tree);
+			}
+		}
 		
 	}
+
+
+	public test(){
+		
+		setSize (1024, 768);
+		setResizable(false);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		//getContentPane().setBackground(Color.GREEN);
+		
+		JLabel grass = new JLabel(new ImageIcon("grass1.png"));
+		add(grass);
+		
+		
+		//create the different sides of the storyboard - the trees that define the area
+		JPanel borderEast = new JPanel();
+		levelBorder(borderEast, "EAST");
+		JPanel borderWest = new JPanel();
+		levelBorder(borderWest, "WEST");
+		JPanel borderNorth = new JPanel();
+		levelBorder(borderNorth, "NORTH");
+		JPanel borderSouth = new JPanel();
+		levelBorder(borderSouth, "SOUTH");
+		
+		setVisible(true);
+	}
+
 	public static void main(String[] arguments){
-		test testLabel = new test();
+		new test();
 	}
 }
