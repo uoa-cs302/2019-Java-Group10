85259127897929fe3bfccae8e4833857da82f045
Lakshya Oberoi <lobe655@aucklanduni.ac.nz>
2019-04-21 15:56:44 +1200

    home complete

--- YT Project/Zelda Shooting Project/src/Home.java
+++ YT Project/Zelda Shooting Project/src/Home.java
@@ -5,63 +5,87 @@
 //Created By: GameWarriors
 //Date Modified: 19/04/19
 
+import java.awt.Canvas;
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.Rectangle;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.awt.image.BufferedImage;
 
-import javax.swing.JButton;
 import javax.swing.JFrame;
 
-public class Home implements ActionListener {
+public class Home extends Canvas {
 	
-	private JButton newGame;
-	public JFrame homeFrame;
-	
-	
-	private BufferedImage backgroundZelda = null;
-	
-
-	
+	public static JFrame homeFrame;
+	Rectangle r;
+	private BufferedImage backgroundZelda = null, startGame = null;
+			
 	public Home() {
-		homeFrame = new JFrame("Zelda Reborn");
-		homeFrame.setSize(1024, 768);
-		homeFrame.setLayout(null);
-		homeFrame.setResizable(false);
-		
-		newGame = new JButton("New Game");//new JButton(new ImageIcon("D:\\icon.png")
-		newGame.setBounds(750, 100, 100, 50);
-		newGame.addActionListener(this);
-		
-		homeFrame.add(newGame);
-
-		homeFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		homeFrame.setVisible(true);
-		
 		
 		BufferedImageLoader loader = new BufferedImageLoader();
 		backgroundZelda = loader.loadImage("/ZeldaBackground.jpg");
+		startGame = loader.loadImage("/start.PNG");
+			
 		
-		//render(0, 0, g);
+		r = new Rectangle();
+		r.setBounds(850, 400, 100, 70);
+		
+		//detect mouse input to buttons
+		addMouseListener(new MouseAdapter(){
+			public void mousePressed(MouseEvent e) {
+				double x = e.getX();
+				double y = e.getY();
+			
+				//start button
+				if ((x > r.getX()) && (x < r.getX() + 100) && (y > r.getY()) && (y < r.getY() + 70)) {
+					homeFrame.dispose();
+					new Game();
+				}
+			}
+		});
+		
+	
 	}
 
-	public static void main(String args []) {		
-		new Home();
+	public static void main(String args []) {
+		
+		homeFrame = new JFrame("Zelda Reborn");
+		Home h = new Home();
+		homeFrame.setSize(1024, 768);
+		//homeFrame.setLayout(null);
+		homeFrame.setResizable(false);
+		
+//		JButton newGame = new JButton("New Game");//new JButton(new ImageIcon("D:\\icon.png")
+//		newGame.setBounds(750, 100, 100, 50);
+//		newGame.addActionListener(this);
+
+		homeFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		homeFrame.setLocationRelativeTo(null);
+		homeFrame.setVisible(true);
+		
+		homeFrame.add(h);		
 	}
 	
+	/*
 	//new game button click - start new game
 	public void actionPerformed(ActionEvent click) {
-		if (click.getSource() == newGame) {
-			//JOptionPane.showMessageDialog(null, "Yay");
+		if (click.getSource() == r) {
+			//JOptionPane.showMessageDialog(null, "Yay"); // above == newGame
 			homeFrame.dispose();
 			new Game();
 		}
+	}*/
+
+
+	public void paint (Graphics g) {
+		//g.drawImage(backgroundZelda, 50, 50, null);
+		g.drawImage(backgroundZelda, 0, 0, 1024, 768, null);
+		g.drawImage(startGame, 850, 400, 100, 70, null);
+		g.setColor(Color.WHITE);
+		g.setFont(new Font("TimesRoman", Font.BOLD, 80)); 
+		g.drawString("ZELDA Reborn", 20, 90);
 	}
-	
-	
-	public void render(int x, int y, Graphics g) {
-		//if character is not moving, draw still frame of animation
-			g.drawImage(backgroundZelda, x, y, null);
-		}
-	
+
 }
